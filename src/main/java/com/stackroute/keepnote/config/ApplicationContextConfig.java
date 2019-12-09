03016233b588a4201ebeae034cb37c74102bb2d4
ubuntu <ubuntu@ip-172-31-33-177.ec2.internal>
2019-12-09 13:46:09 +0000

    completed

--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -1,5 +1,21 @@
 package com.stackroute.keepnote.config;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /*This class will contain the application-context for the application. 
  * Define the following annotations:
  * @Configuration - Annotating a class with the @Configuration indicates that the 
@@ -12,6 +28,10 @@ package com.stackroute.keepnote.config;
  *                  
  * 
  * */
+@Configuration
+@ComponentScan(basePackages="com.stackroute.keepnote")
+@EnableWebMvc
+@EnableTransactionManagement
 
 public class ApplicationContextConfig {
 
@@ -20,7 +40,19 @@ public class ApplicationContextConfig {
 	 * dataSource. To create the DataSource bean, we need to know: 1. Driver class
 	 * name 2. Database URL 3. UserName 4. Password
 	 */
+	@Bean
+	@Autowired
+	public DataSource getDataSource() {
+		
+		BasicDataSource dataSource = new BasicDataSource();
 
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/test"
+				+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+ 		return dataSource;
+	}
 	/*
 	 * Use this configuration while submitting solution in hobbes.
 	 * dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -35,11 +67,27 @@ public class ApplicationContextConfig {
 	 * create a getter for Hibernate properties here we have to mention 1. show_sql
 	 * 2. Dialect 3. hbm2ddl
 	 */
-
 	/*
 	 * Define the bean for SessionFactory. Hibernate SessionFactory is the factory
 	 * class through which we get sessions and perform database operations.
 	 */
+	@Bean
+    @Autowired
+    public LocalSessionFactoryBean getSessionFactory(DataSource dataSource) throws IOException {
+
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource);
+		sessionFactoryBean.setPackagesToScan("com.stackroute.keepnote.model");
+		Properties properties = new Properties();
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+//		sessionFactoryBean.setPackagesToScan("com.stackroute.keepnote.model");
+		sessionFactoryBean.setHibernateProperties(properties);
+		sessionFactoryBean.afterPropertiesSet();
+		return sessionFactoryBean;
+	}
+	
 
 	/*
 	 * Define the bean for Transaction Manager. HibernateTransactionManager handles
@@ -49,5 +97,12 @@ public class ApplicationContextConfig {
 	 * JDBC too. HibernateTransactionManager allows bulk update and bulk insert and
 	 * ensures data integrity.
 	 */
+		@Bean
+	   	@Autowired
+	   	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
 
+	   		HibernateTransactionManager transaction = new HibernateTransactionManager();
+	   		transaction.setSessionFactory(sessionFactory);
+	   		return transaction;
+	   	}
 }
